d43a12e8c67949593117dcf686ee9dbf81a9c718
Hannes Schaletzky <hannes.schaletzky@digitalservice.bund.de>
2022-08-19 19:12:59 +0200

    cleaned up

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
 console.log("hello from background.js")
 
-
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
+  });
+});
 
--- popup.js
+++ popup.js
@@ -1,41 +1,32 @@
-let btn = document.getElementById("initButton");
-
 // set label text
 let label = document.getElementById("selectedLabels");
-label.innerText = "jdosjaoisa"
+label.innerText = "This is a label"
 
+// add event listener to button
+let btn = document.getElementById("initButton");
 btn.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: sayHi,
+    function: onClick,
   });
 });
 
-function sayHi() {
-  console.log("HI")
-
+function onClick() {
+  // read query params from URL
   const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
   });    
   console.log(params.label)
 
-  let key = "Key"
-  let value = "TEST"
-  // chrome.storage.sync.set({key: value}, function() {
-  //   console.log('Value is set to ' + value);
-  // });
-    
+  // set value in storage
+  chrome.storage.sync.set({key: "ABC"});
+  
+  // get value from storage
   chrome.storage.sync.get(['key'], function(result) {
     console.log('Value currently is ' + result.key);
   });
-
-
-  document.addEventListener("click", function(){
-    console.log("CLICKED")
-  });
-
 
   // set query params without reload
   if ('URLSearchParams' in window) {
@@ -44,6 +35,7 @@ function sayHi() {
       var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
       history.pushState(null, '', newRelativePathQuery);
   }
+
   // set query params with reload
   // if ('URLSearchParams' in window) {
   //   var searchParams = new URLSearchParams(window.location.search);
@@ -51,6 +43,3 @@ function sayHi() {
   //   window.location.search = searchParams.toString();
   // }
 }
-
-
-//data-test-id="filters.common.ui.list.menu.list"
